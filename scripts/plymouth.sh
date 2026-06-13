7d8eb5fdef77fb92a06b26fd67047bb4cd56d3d8
i am clang <namb20994@gmail.com>
2026-06-13 16:06:01 +0700

    Update plymouth.sh to set default theme
    Add patch for setting default Plymouth theme and updating initramfs.

--- scripts/plymouth.sh
+++ scripts/plymouth.sh
@@ -1,6 +1,10 @@
-!/bin/bash
+#!/bin/bash
 
 # Spark Linux
 # This is a symlink, thing can break in future
 chmod +x ../plymouth/install.sh
 ../plymouth/install.sh
+
+# patch 
+plymouth-set-default-theme sparkPlymouth
+update-initramfs -u
